e9a9da1fde03824af801a9a34c8cf5b838cecd4f
MM <143347704+MacielMaximiliano@users.noreply.github.com>
2023-11-30 13:57:53 -0300

    s

---
+++ PB2-Parcial-TM-MacielMaximiliano/test/ar/edu/unlam/pb2/Parcial01/test.java
@@ -0,0 +1,14 @@
+package ar.edu.unlam.pb2.Parcial01;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class test {
+
+	@Test
+	public void QueSePuedaAgregarUnaCompra() {
+		
+	}
+
+}
